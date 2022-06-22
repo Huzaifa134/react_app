548810d8cebb9282932c9f9296f615b51b749a05
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2022-06-22 13:48:21 -0700

    Updates

---
+++ precache-manifest.b437d70686519f297995d13c20560677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "944f5daa2f117227d2f8",
+    "url": "/text-utils/static/css/main.5ded8737.chunk.css"
+  },
+  {
+    "revision": "944f5daa2f117227d2f8",
+    "url": "/text-utils/static/js/main.944f5daa.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/text-utils/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/text-utils/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "cc7693ea043117531dae",
+    "url": "/text-utils/static/js/runtime~main.cc7693ea.js"
+  },
+  {
+    "revision": "d465788674c9cac76edfab7401f56942",
+    "url": "/text-utils/index.html"
+  }
+];
